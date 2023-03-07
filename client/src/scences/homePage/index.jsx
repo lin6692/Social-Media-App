9724ab26b0a901746d5d5e7e75d64e9b9ced0f90
Lin Liu <lin_6066@yahoo.com>
2023-03-06 21:07:47 -0500

    added profile page

--- client/src/scences/homePage/index.jsx
+++ client/src/scences/homePage/index.jsx
@@ -4,39 +4,43 @@ import Navbar from "scences/navbar";
 import UserWidget from "scences/widgets/UserWidget";
 import MyPostWidget from "scences/widgets/MyPostWidget";
 import PostsWidget from "scences/widgets/PostsWidget";
+import AdvertWidget from "scences/widgets/AdvertWidget";
+import FriendListWidget from "scences/widgets/FriendListWidget";
 
 const HomePage = () => {
-    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-    const { _id, picturePath } = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
 
-    return (
+  return (
     <Box>
-        <Navbar />
-        <Box
-            width="100%"
-            padding="2rem 6%"
-            display={isNonMobileScreens ? "flex" : "block"}
-            gap="0.5rem"
-            justifyContent="space-between"
-        >
-            <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
-                <UserWidget userId={_id} picturePath={picturePath} />
-            </Box>
-            <Box
-                flexBasis={isNonMobileScreens ? "42%" : undefined}
-                mt={isNonMobileScreens ? undefined : "2rem"}
-            >
-                <MyPostWidget picturePath={picturePath} />
-                <PostsWidget userId={_id} />
-            </Box>
-            {isNonMobileScreens && (
-                <Box flexBasis="26%">
-
-                </Box>
-            )}
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
         </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+      </Box>
     </Box>
-    );
+  );
 };
 
 export default HomePage;
