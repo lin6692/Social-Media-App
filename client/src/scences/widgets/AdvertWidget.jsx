da1887185c3ae962fa07b0e3600f13960593f53b
Lin Liu <lin_6066@yahoo.com>
2023-03-11 16:41:24 -0500

    modified file structure and added data

--- client/src/scences/widgets/AdvertWidget.jsx
+++ client/src/scences/widgets/AdvertWidget.jsx
@@ -28,9 +28,7 @@ const AdvertWidget = () => {
         <Typography color={main}>Welcome to my social app project</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        This social website is built using the MERN stack, it leverages the power of 
-        <Typography sx={{ display: 'inline', fontWeight:"bold" }}> MongoDB, Express, React, and Node.js </Typography>
-        to deliver a fast, responsive, and intuitive user experience.
+        This social website is built using the MERN stack, it leverages the power of MongoDB, Express, React, and Node.js to deliver a fast, responsive, and intuitive user experience.
       </Typography>
     </WidgetWrapper>
   );
--- server/index.js
+++ server/index.js
@@ -64,8 +64,8 @@ mongoose
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
-    // Comment.insertMany(comments);
+    User.insertMany(users);
+    Post.insertMany(posts);
+    Comment.insertMany(comments);
   })
   .catch((error) => console.log(`${error} did not connect`));
