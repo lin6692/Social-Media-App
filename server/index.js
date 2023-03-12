2f8b8b0b1320d39adfdae3c38d1edfc0bfb3f67f
Lin Liu <lin_6066@yahoo.com>
2023-03-12 12:14:15 -0400

    set static folder

--- server/index.js
+++ server/index.js
@@ -53,6 +53,15 @@ app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
